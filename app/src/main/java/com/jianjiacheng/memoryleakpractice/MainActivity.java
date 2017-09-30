af73d7e4f787be94f16a3a55ba583df9f4bdf3f7
JasonChien <s124202468@gmail.com>
2017-09-30 15:29:12 +0800

    Go login

--- app/src/main/java/com/jianjiacheng/memoryleakpractice/MainActivity.java
+++ app/src/main/java/com/jianjiacheng/memoryleakpractice/MainActivity.java
@@ -2,6 +2,7 @@ package com.jianjiacheng.memoryleakpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        findViewById(R.id.my_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoLogin() ;
+            }
+        });
     }
 
     public void gotoLogin(){
