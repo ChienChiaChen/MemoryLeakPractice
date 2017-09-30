c49207770857f0f44d850e6d3d11d199b211f98d
JasonChien <s124202468@gmail.com>
2017-09-30 15:22:10 +0800

    Git init

---
+++ app/src/main/java/com/jianjiacheng/memoryleakpractice/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jianjiacheng.memoryleakpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+//    public void gotoLogin(){
+//        startActivity(LoginActivity.from(this));
+//    }
+}
